2d6452935cc5b902fb196bf996d3415e5772f0a6
BOSSA Mickael <mickael.bossa@outlook.com>
2022-11-30 15:00:53 +0100

    Add first screen

--- components/ui/Header/Header.tsx
+++ components/ui/Header/Header.tsx
@@ -2,17 +2,35 @@
 import styles from './Header.module.css';
 
 // Next
-import Link from "next/link"
+import Link from 'next/link';
 
 export default function Header() {
-  return (
-    <nav className='container'>
-        <ul className={styles.navlinks}>
-            <li><Link className={styles.navlink} href='#'>01- Skills</Link></li>
-            <li><Link className={styles.navlink} href='#'>02- Project</Link></li>
-            <li><Link className={styles.navlink} href='#'>03- Contact</Link></li>
-            <li><Link className={styles.navlink} href='#'>04- Blog</Link></li>
-        </ul>
-    </nav>
-  )
+    return (
+        <header className="container">
+            <nav>
+                <ul className={styles.navlinks}>
+                    <li>
+                        <Link className={styles.navlink} href="#">
+                            01- Skills
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={styles.navlink} href="#">
+                            02- Project
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={styles.navlink} href="#">
+                            03- Contact
+                        </Link>
+                    </li>
+                    <li>
+                        <Link className={styles.navlink} href="#">
+                            04- Blog
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
---
+++ components/ui/SocialNetworkBtn/SocialNetworkBtn.tsx
@@ -0,0 +1,20 @@
+// Style
+import styles from './SocialNetworkBtn.module.css';
+
+// Next
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function SocialNetworkBtn({src, title}: {src:string, title: string}) {
+    return (
+        <Link href="#" className={styles.btnContainer}>
+            <Image
+                src={src}
+                height={36}
+                width={36}
+                alt={`${title} icon`}
+            />
+            <p className={styles.btnTxt}>{title}</p>
+        </Link>
+    );
+}
